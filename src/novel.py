55e6f6417b593ae38e969b836fd505b1835a560e
Monik3x <cristianstefan2005@gmail.com>
2026-08-09 20:43:50 +0300

    forgot

--- src/novel.py
+++ src/novel.py
@@ -1,8 +1,34 @@
 import logging
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Comment
 from src.helper import normalize_url
 
 logger = logging.getLogger("pia_scrap")
+
+EPUB_ALLOWED_TAGS = {
+    "a", "b", "blockquote", "br", "caption", "code", "col", "colgroup",
+    "dd", "del", "div", "dl", "dt", "em", "figcaption", "figure", "h1",
+    "h2", "h3", "h4", "h5", "h6", "hr", "i", "img", "ins", "li", "ol",
+    "p", "pre", "q", "ruby", "rp", "rt", "s", "small", "span", "strong",
+    "sub", "sup", "table", "tbody", "td", "tfoot", "th", "thead", "tr",
+    "u", "ul",
+}
+EPUB_DROP_CONTENT_TAGS = {
+    "applet", "audio", "button", "canvas", "embed", "form", "iframe", "input",
+    "link", "meta", "noscript", "object", "script", "select", "source", "style",
+    "svg", "template", "textarea", "video",
+}
+EPUB_GLOBAL_ATTRIBUTES = {"class", "dir", "id", "lang", "title"}
+EPUB_TAG_ATTRIBUTES = {
+    "a": {"href"},
+    "col": {"span"},
+    "img": {"alt", "height", "src", "width"},
+    "li": {"value"},
+    "ol": {"reversed", "start", "type"},
+    "q": {"cite"},
+    "td": {"colspan", "rowspan"},
+    "th": {"colspan", "rowspan", "scope"},
+}
+SAFE_LINK_SCHEMES = {"http", "https", "mailto"}
 
 # ----------------------------
 # Novelpia Novel & Episodes Fetcher
@@ -15,11 +41,32 @@ def html_from_episode_text(raw_html: str) -> str:
         lazy_src = img.get("data-src") or img.get("data-original") or img.get("data-lazy-src")
         if lazy_src:
             img["src"] = lazy_src
-        if "style" in img.attrs:
-            del img["style"]
-        for attribute in ("srcset", "data-srcset"):
-            if attribute in img.attrs:
-                del img[attribute]
+
+    for comment in soup.find_all(string=lambda text: isinstance(text, Comment)):
+        comment.extract()
+
+    for tag in list(soup.find_all(True)):
+        if not tag.name:
+            continue
+        name = tag.name.lower()
+        if name in EPUB_DROP_CONTENT_TAGS:
+            tag.decompose()
+        elif name not in EPUB_ALLOWED_TAGS:
+            tag.unwrap()
+
+    for tag in soup.find_all(True):
+        allowed_attributes = EPUB_GLOBAL_ATTRIBUTES | EPUB_TAG_ATTRIBUTES.get(tag.name, set())
+        for attribute in list(tag.attrs):
+            if attribute.lower() not in allowed_attributes:
+                del tag.attrs[attribute]
+
+        if tag.name == "a" and tag.get("href"):
+            href = tag["href"].strip()
+            scheme = href.split(":", 1)[0].lower() if ":" in href else ""
+            if not (href.startswith(("#", "/")) or scheme in SAFE_LINK_SCHEMES):
+                del tag["href"]
+
+    for img in soup.find_all("img"):
         if img.get("src"):
             img["src"] = normalize_url(img["src"])
 
@@ -53,7 +100,7 @@ def fetch_novel_and_episodes(client, novel_id, max_chapters=None):
     list_result = data_list.get("result") or {}
     if not isinstance(list_result, dict):
         raise ValueError(f"Novel {novel_id} returned an invalid episode response.")
-    ep_list = list_result.get("list") or []
+    ep_list = list_result.get("list", [])
     if not isinstance(ep_list, list):
         raise ValueError(f"Novel {novel_id} returned an invalid episode list.")
 
